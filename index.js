82389f92488a61f7e8c10cbfdc33b0d0c5536a3f
Aulianurfitria15 <aulianurfitria17@gmail.com>
2025-10-27 12:17:02 +0700

    tambah route untuk menghapus data komik berdasarkan id

--- index.js
+++ index.js
@@ -59,3 +59,18 @@ app.put("/komik/:id", async (req, res) => {
         res.status(500).send(error);
     }
 })
+
+app.delete("/komik/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try{
+        const komik = await db.Komik.findByPk(id);
+        if(!komik){
+            return res.status(404).send({message: "Komik tidak tersedia"});
+        }
+        await komik.destroy();
+        res.send({message: "Komik berhasil dihapus", komik});
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
